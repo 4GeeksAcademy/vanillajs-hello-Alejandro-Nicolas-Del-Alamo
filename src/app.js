7b234266cf48a68bc6c620031bec36ff5ec3bf7c
Alejandro <alejandronicolasdelalamo@gmail.com>
2024-11-19 16:59:57 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -9,3 +9,27 @@ window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
 };
+
+let pronoun = ["the", "our"];
+let adj = ["great", "big"];
+let noun = ["jogger", "racoon"];
+
+function generateDomains(pronouns, adjectives, nouns) {
+  let domains = [];
+
+  // Bucles anidados para combinar las palabras
+  for (let p of pronouns) {
+    for (let a of adjectives) {
+      for (let n of nouns) {
+        let domain = `${p}${a}${n}.com`;
+        domains.push(domain);
+      }
+    }
+  }
+
+  return domains;
+}
+
+let generatedDomains = generateDomains(pronoun, adj, noun);
+console.log("Generated Domains:");
+console.log(generatedDomains);
